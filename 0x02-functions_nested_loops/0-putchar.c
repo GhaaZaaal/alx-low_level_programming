4b43677ed26df9298dc506465524a6303ad6eab9
GhaaZaaal <aghazal88@gmail.com>
2023-10-13 08:29:09 -0700

    1st project & new dir

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'C program that prints a string'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("_putchar");
+
+	return (0);
+}
