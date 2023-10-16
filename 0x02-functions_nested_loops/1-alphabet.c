bd871a10e6f7482df2d1f2bfad20038340172b1b
GhaaZaaal <aghazal88@gmail.com>
2023-10-15 20:05:12 -0700

    misunderstanding all time I need more focus hot fix task 2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,19 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
- * Description: 'C program that print the alphabets
- *		in lower case using external function'
- *
- * Return: Always 0 (Success)
+ * print_alphabet - 'c external function to print the alphabets in lowercase'
+ * 
+ * Return: 0 (Success)
 */
 
-int main(void)
+void print_alphabet(void)
 {
-	print_alphabet();
-	return (0);
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+
+	_putchar('\n');
 }
