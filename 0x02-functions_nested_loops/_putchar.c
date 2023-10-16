a87f566efd56331764790d49ec18d47be51b3481
GhaaZaaal <aghazal88@gmail.com>
2023-10-15 19:32:54 -0700

    first task i hope its good

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - prints 'char c'
+ *
+ * Return: 1 (Success).
+ * Return: -1 on error
+*/
+
+int _putchar(char c)
+{
+	return (write(1,&c,1));
+}
