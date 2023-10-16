dd808cb2792818d504e3195047e41a1fafb7ef49
GhaaZaaal <aghazal88@gmail.com>
2023-10-16 04:17:20 -0700

    the last 2 mandatory

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - c function that add two integers
+ *
+ * @n: the first variable of the addition function
+ * @y: the second variable of the addition function
+ *
+ * Return: the addition of the two integers
+*/
+int add(int n, int y)
+{
+	return (n + y);
+}
