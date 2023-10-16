b5ad6df9bbac15de91a140a6299f6101a4cfe64d
GhaaZaaal <aghazal88@gmail.com>
2023-10-15 20:33:39 -0700

    third hot fix and fourth

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: 'C program that check the case of the letter
+ *		using external function'
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int _islower(int c);
+	return (0);
+}
