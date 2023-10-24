976e7c83884c5f2da5b6785bd8ad8d3a1f767aae
GhaaZaaal <aghazal88@gmail.com>
2023-10-23 21:20:16 -0700

    the first adv

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * rot13 - C func that encodes a string using rot13
+ *
+ * @s: the string to be encoded
+ *
+ * Return: the encoded string
+*/
+
+char *rot13(char *s)
+{
+	int i;
+
+	char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	char *ps = s;
+
+	while (*s)
+	{
+		for (i = 0; i <= 52; i++)
+		{
+			if (*s == rot13[i])
+			{
+				*s = ROT13[i];
+				break;
+			}
+		}
+		s++;
+	}
+	return (ps);
+}
