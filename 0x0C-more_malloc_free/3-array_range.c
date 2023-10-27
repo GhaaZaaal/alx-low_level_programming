994d2467791c5751670d7f822300db7629d8ecfb
GhaaZaaal <aghazal88@gmail.com>
2023-10-27 14:37:36 -0700

    4th task

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -3,13 +3,29 @@
 /**
  * array_range - C func that creates an array of int
  *
- * @min:
- * @max:
+ * @min: the minimum value of the array
+ * @max: the maximum value of the array
  *
- * Return:
+ * Return: the array
 */
 
 int *array_range(int min, int max)
 {
+	int *a;
+	int i, x;
 
+	if (min > max)
+		return (NULL);
+
+	x = mix - min + 1;
+
+	a = malloc(sizeof(int) * x);
+
+	if (a == NULL)
+		return (NULL);
+
+	for (i = 0; i < x; i++)
+		a[i] = min++;
+
+	return (a);
 }
