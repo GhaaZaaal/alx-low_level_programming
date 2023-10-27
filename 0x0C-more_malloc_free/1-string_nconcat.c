36cbb387a308b4f64c323cd2ec8e949b9dbbbd52
GhaaZaaal <aghazal88@gmail.com>
2023-10-27 11:47:25 -0700

    second task

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,15 +3,40 @@
 /**
  * string_nconcat - C func that concatenates two strings
  *
- * @s1:
- * @s2:
- * @n:
+ * @s1: the pointer of the first string
+ * @s2: the pointer of the second string
+ * @n: the number of bytes
  *
- * Return:
+ * Return: if fail return null if n >= s2 return s2
 */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	char *s;
+	unsigned int i, l = n;
 
-	return ();
+	if (s1 == NULL)
+		return (NULL);
+	if (s2 == NULL)
+		return (NULL);
+
+	for (i = 0; s1[i]; i++)
+		l++;
+
+	s = malloc(sizeof(char) * (l + 1));
+
+	if (s == NULL)
+		return (NULL);
+
+	l = 0;
+
+	for (i = 0; s1[i]; i++)
+		s[l++] = s1[i];
+
+	for (i = 0; s2[i] && i < n; i++)
+		s[l++] = s2[i];
+
+	s[l] = '\0';
+
+	return (s);
 }
