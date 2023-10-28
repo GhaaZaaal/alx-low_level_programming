92c069552419e499d2cb6b1471ad04d4a43cdaff
GhaaZaaal <aghazal88@gmail.com>
2023-10-28 01:29:12 -0700

    fixing 2nd

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -16,6 +16,6 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	int *last = array + size - 1;
 
 	if (array && size && action)
-		while (array <= lasst)
+		while (array <= last)
 			action(*array++);
 }
