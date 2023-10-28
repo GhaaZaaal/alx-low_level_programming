58254c69f194ad21b5ed6ae639e2c95d59960339
GhaaZaaal <aghazal88@gmail.com>
2023-10-28 01:09:10 -0700

    fixing tha last

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,11 +1,17 @@
 #include "dog.h"
 
-/***/
+/**
+ * free_dog - C func that free the mem of a struct
+ *
+ * @d: the pointer to the struct
+ *
+ * Return: void
+*/
 
 void free_dog(dog_t *d)
 {
 	if (d == NULL)
-		return (NULL);
+		return;
 
 	free(d->owner);
 	free(d->name);
