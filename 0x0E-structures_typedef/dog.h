c188e902d5baa2088cd437996b229655a487cede
GhaaZaaal <aghazal88@gmail.com>
2023-10-28 00:02:00 -0700

    first task with empty all files

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct
+ *
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog's owner
+ *
+ * Description: a struct to dog name age owner
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif /* DOG_H */
