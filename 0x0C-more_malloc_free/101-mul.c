e18af19f554a869e995ce0bf6dd628535acfb76f
GhaaZaaal <aghazal88@gmail.com>
2023-10-27 19:08:43 -0700

    the last adv

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,94 @@
+#include "main.h"
+
+/**
+ * putstr - C func that prints a string
+ *
+ * @str: the pointer of the string to be printed
+ *
+ * Return: the string
+*/
+
+void putstr(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
+
+/**
+ * number - C func that convert a string to integer
+ *
+ * @s: the pointer to the string to be converted
+ *
+ * Return: the integer
+*/
+
+int number(const char *s)
+{
+	int repositive = 1;
+	unsigned long int numb1, i, j;
+
+	for (numb1 = 0; !(s[numb1] >= 48 && s[numb1] <= 57); numb1++)
+	{
+		if (s[numb1] == '-')
+			repositive *= -1;
+	}
+
+	for (i = numb1; s[i] >= 48 && s[i] <= 57; i++)
+	{
+		j *= 10;
+		j += (s[i] - 48);
+	}
+
+	return (repositive * j);
+}
+
+/**
+ * pr_int - C func that prints an integer
+ *
+ * @x: the integer we need
+ *
+ * Return: void
+*/
+
+void pr_int(unsigned long int x)
+{
+	unsigned long int i, j, rechar = 1;
+
+	for (i = 0; x / rechar > 9; i++, rechar += 10)
+		;
+
+	for (; rechar >= 1; x %= rechar, rechar /= 10)
+	{
+		rechar = x / j;
+		_putchar('0' + j);
+	}
+}
+
+/**
+ * main - C prog that print the result of the multiplication of two numbers
+ *
+ * @argc: the integers
+ * @argv: the array of the stored ints
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+
+	if (argc != 3)
+	{
+		_putstr("Error ");
+		exit(98);
+	}
+	pr_int(number(argv[1]) * number(argv[2]));
+	_putchar('\n');
+
+	return (0);
+}
