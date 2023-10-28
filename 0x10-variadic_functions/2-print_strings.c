2469f0f4ebace9fca817cdca5b4c27aabb0f0003
GhaaZaaal <aghazal88@gmail.com>
2023-10-28 04:28:50 -0700

    the 3rd

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - C func that print numbers with separator in between
+ *
+ * @separator: the separator string used
+ * @n: the number of arguments
+ * @...: the integers that will be printed
+ *
+ * Return: void
+*/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int i = n;
+	char *s;
+	va_list ap;
+
+	if (!n)
+	{
+		printf("\n");
+		return;
+	}
+	va_start(ap, n);
+	while (i--)
+		printf("%s%s", (s = va_arg(ap, char *)) ? s : "(nil)",
+				i ? (separator ? separator : "") : "\n");
+	va_end(ap);
+}
