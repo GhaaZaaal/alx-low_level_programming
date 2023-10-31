3d6ee671fd823066a10cae8eaf3d7f2ca83c5e27
GhaaZaaal <aghazal88@gmail.com>
2023-10-30 18:15:23 -0700

    3rd fix 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,35 +17,35 @@
 
 int main(int ec, char **ev)
 {
-	int file_from = 0, file_to = 0;
+	int from_fd = 0, to_fd = 0;
 	ssize_t b;
 	char buf[READ_BUF_SIZE];
 
 	if (ec != 3)
 		dprintf(STDERR_FILENO, USAGE), exit(97);
 
-	file_from = open(ev[1], O_RDONLY);
-	if (file_from == -1)
+	form_fd = open(ev[1], O_RDONLY);
+	if (form_fd == -1)
 		dprintf(STDERR_FILENO, ERR_NOREAD, ev[1]), exit(98);
 
-	file_to = open(ev[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
-	if (file_to == -1)
+	to_fd = open(ev[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
+	if (to_fd == -1)
 		dprintf(STDERR_FILENO, ERR_NOWRITE, ev[2]), exit(99);
 
-	while ((b = read(file_from, buf, READ_BUF_SIZE)) > 0)
-		if (write(file_to, buf, b) != b)
+	while ((b = read(from_fd, buf, READ_BUF_SIZE)) > 0)
+		if (write(to_fd, buf, b) != b)
 			dprintf(STDERR_FILENO, ERR_NOWRITE, ev[2]), exit(99);
 
 	if (b == -1)
 		dprintf(STDERR_FILENO, ERR_NOREAD, ev[1]), exit(98);
 
-	file_from = close(file_from);
-	file_to = close(file_to);
+	from_fd = close(from_fd);
+	to_fd = close(to_fd);
 
-	if (file_from)
-		dprintf(STDERR_FILENO, ERR_NOCLOSE, file_from), exit(100);
-	if (file_to)
-		dprintf(STDERR_FILENO, ERR_NOCLOSE, file_to), exit(100);
+	if (from_fd)
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, from_fd), exit(100);
+	if (to_fd)
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, from_fd), exit(100);
 
 	return (EXIT_SUCCESS);
 }
