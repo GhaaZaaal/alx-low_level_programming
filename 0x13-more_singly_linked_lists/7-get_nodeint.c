26674d68e0b83fcceae8cdd944b51ae623461516
GhaaZaaal <aghazal88@gmail.com>
2024-02-06 20:30:46 +0200

    forgot to make the i as unsigned as the index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,7 +9,7 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	int i;
+	unsigned int i;
 	listint_t *ptr = head;
 
 	if (ptr != NULL)
@@ -20,4 +20,26 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		}
 	}
 	return (ptr);
+}
+
+int main(void)
+{
+    listint_t *head;
+    listint_t *node;
+
+    head = NULL;
+    add_nodeint_end(&head, 0);
+    add_nodeint_end(&head, 1);
+    add_nodeint_end(&head, 2);
+    add_nodeint_end(&head, 3);
+    add_nodeint_end(&head, 4);
+    add_nodeint_end(&head, 98);
+    add_nodeint_end(&head, 402);
+    add_nodeint_end(&head, 1024);
+    print_listint(head);
+    node = get_nodeint_at_index(head, 5);
+    printf("%d\n", node->n);
+    print_listint(head);
+    free_listint2(&head);
+    return (0);
 }
