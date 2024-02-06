84d6a9d490c73681da8c63d941d68941811475a4
GhaaZaaal <aghazal88@gmail.com>
2024-02-06 21:03:04 +0200

    checks for the ptr.next is null

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,6 +22,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	for (i = 0; i < idx; i++)
 	{
 		tmp = ptr;
+		if (ptr->next == NULL)
+		{
+			return (NULL);
+		}
 		ptr = ptr->next;
 	}
 	tmp->next = new;
